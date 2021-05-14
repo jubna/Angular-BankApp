6800efa3e986929e4c8b653578a68de4b87e4ab5
jubna <jubi912@gmail.com>
2021-05-14 10:32:11 +0300

    event binding by passing argument as  (event)='fun-call()'

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -7,9 +7,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+
+      aim="your perfect banking partner"
+      acno="Enter ur ac no"
+      password=""
+
+      accountDetails = {
+        1000: { acno: 1000,  username: "userone", password: "userone", balance: 50000 },
+        1001: { acno: 1001,  username: "usertwo", password: "usertwo", balance: 5000 },
+        1002: { acno: 1002,  username: "userthree", password: "userthree", balance: 10000 },
+        1003: { acno: 1003,  username: "userfour", password: "userfour", balance: 6000 }
+      };
   constructor() { }
 
   ngOnInit(): void {
   }
-
+  login(){
+    var acno=this.acno;
+    var pwd=this.password;
+    let users=this.accountDetails;
+    if(acno in users){
+      if (pwd == users[acno]["password"]) {
+        alert("success")
+      }
+      else{
+       alert("invalid username or password");
+      }
+    }
+    else{
+     alert("invalid account number");
+    }
+    }
+  
+  accnoChange(event:any){
+    this.acno=event.target.value;
+  console.log(this.acno);
+  }
+  pwdChange(event:any){
+    this.password=event.target.value;
+    console.log(this.password);
+  }  
+ 
 }
